1b9fb148abe944b857470d1fd4491d6b9215af0d
Anmei Liu <anmliu@ucdavis.edu>
2020-06-09 16:14:14 -0700

    more tests and tests for warnings

--- tests/testthat/test-blblm-fit.R
+++ tests/testthat/test-blblm-fit.R
@@ -1,7 +1,7 @@
 test_that("sequential, data given directly", {
   future::plan(future::sequential)
   mtcars <- read.csv("data/mtcars.csv")
-  b <- blblm(mpg ~ wt, mtcars, B = 100)
+  expect_warning(b <- blblm(mpg ~ wt, mtcars, B = 100))
   expect_s3_class(b, "blblm")
   co <- coef(b)
   expect_equal(length(co), 2)
@@ -21,7 +21,7 @@ test_that("parallelized, data given directly", {
 test_that("sequential, data read from files", {
   future::plan(future::sequential)
   filepaths = c("data/mtcars1.csv", "data/mtcars2.csv")
-  b <- blblm(mpg ~ wt, filepaths = filepaths, B = 100)
+  expect_warning(b <- blblm(mpg ~ wt, filepaths = filepaths, B = 100))
   expect_s3_class(b, "blblm")
   co <- coef(b)
   expect_equal(length(co), 2)
@@ -31,9 +31,23 @@ test_that("sequential, data read from files", {
 test_that("parallelized, data read from files", {
   suppressWarnings(future::plan(future::multiprocess))
   filepaths = c("data/mtcars1.csv", "data/mtcars2.csv")
-  b <- blblm(mpg ~ wt, filepaths = filepaths, B = 100)
+  b <- blblm(mpg ~ wt, filepaths = filepaths, m = 2, B = 100)
   expect_s3_class(b, "blblm")
   co <- coef(b)
   expect_equal(length(co), 2)
   expect_type(sigma(b), "double")
+})
+
+test_that("respects use_plan = FALSE", {
+  future::plan(future::sequential)
+  filepaths = c("data/mtcars1.csv", "data/mtcars2.csv")
+  b <- blblm(mpg ~ wt, filepaths = filepaths, m = 2, B = 100, use_plan = FALSE)
+  expect_s3_class(b, "blblm")
+})
+
+test_that("respects file split over m", {
+  future::plan(future::sequential)
+  filepaths = c("data/mtcars1.csv", "data/mtcars2.csv")
+  expect_warning(b <- blblm(mpg ~ wt, filepaths = filepaths, B = 100, use_plan = FALSE))
+  expect_s3_class(b, "blblm")
 })
