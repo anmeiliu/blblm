73534ad0027ee3fa88a06a4d8e3ceab7f693b3aa
Anmei Liu <anmliu@ucdavis.edu>
2020-06-10 12:08:10 -0700

    extra test case for multivariate

--- tests/testthat/test-blbglm-fit.R
+++ tests/testthat/test-blbglm-fit.R
@@ -55,6 +55,16 @@ test_that("respects file split over m", {
 test_that("logistic regression works", {
   future::plan(future::sequential)
   iris <- read.csv("data/iris_subset.csv")
+  expect_warning(b <- blbglm(Species ~ Sepal.Length, family = binomial, data = iris, B = 100, use_plan = FALSE))
+  expect_s3_class(b, "blbglm")
+  co <- coef(b)
+  expect_equal(length(co), 2)
+  expect_type(sigma(b), "double")
+})
+
+test_that("multivariate formula works", {
+  future::plan(future::sequential)
+  iris <- read.csv("data/iris_subset.csv")
   expect_warning(b <- blbglm(Species ~ Sepal.Length + Sepal.Width, family = binomial, data = iris, B = 100, use_plan = FALSE))
   expect_s3_class(b, "blbglm")
   co <- coef(b)
