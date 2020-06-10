229af503cc25023f4a723e1d53fcae9ffb81a40f
Anmei Liu <anmliu@ucdavis.edu>
2020-06-10 14:44:16 -0700

    reorganize and add tests

---
+++ tests/testthat/test-blbglm-fit-opt.R
@@ -0,0 +1,29 @@
+future::plan(future::sequential)
+
+mtcars <- read.csv("data/mtcars.csv")
+iris <- read.csv("data/iris_subset.csv")
+
+test_that("respects use_plan = FALSE", {
+  filepaths = c("data/mtcars1.csv", "data/mtcars2.csv")
+  b <- blbglm(mpg ~ wt, filepaths = filepaths, m = 2, B = 100, use_plan = FALSE)
+  expect_s3_class(b, "blbglm")
+})
+
+test_that("respects file split over m", {
+  filepaths = c("data/mtcars1.csv", "data/mtcars2.csv")
+  expect_warning(b <- blbglm(mpg ~ wt, filepaths = filepaths, B = 100, use_plan = FALSE))
+  expect_s3_class(b, "blbglm")
+})
+
+test_that("respects min_subsample_size and even_split", {
+  # produces NaN due to subsamples being too small to calculate sigma
+  b <- blbglm(mpg ~ wt, data = mtcars, m = 20, B = 100, min_subsample_size = 1, use_plan = FALSE)
+  expect_equal(sigma(b), NaN)
+  # all of these results should be able to calculate non-NaN sigma
+  b <- blbglm(mpg ~ wt, data = mtcars, m = 10, B = 100, min_subsample_size = 3, use_plan = FALSE)
+  expect_gt(sigma(b), 0)
+  b <- blbglm(mpg ~ wt, data = mtcars, m = 10, B = 100, even_split = TRUE, use_plan = FALSE)
+  expect_gt(sigma(b), 0)
+  expect_warning(b <- blbglm(mpg ~ wt, data = mtcars, m = 10, B = 100, min_subsample_size = 3, even_split = TRUE, use_plan = FALSE))
+  expect_gt(sigma(b), 0)
+})
--- tests/testthat/test-blbglm-fit.R
+++ tests/testthat/test-blbglm-fit.R
@@ -1,16 +1,9 @@
-test_that("sequential, data given directly", {
-  future::plan(future::sequential)
-  mtcars <- read.csv("data/mtcars.csv")
-  expect_warning(b <- blbglm(mpg ~ wt, data = mtcars, B = 100))
-  expect_s3_class(b, "blbglm")
-  co <- coef(b)
-  expect_equal(length(co), 2)
-  expect_type(sigma(b), "double")
-})
+suppressWarnings(future::plan(future::multiprocess))
+
+mtcars <- read.csv("data/mtcars.csv")
+iris <- read.csv("data/iris_subset.csv")
 
 test_that("parallelized, data given directly", {
-  suppressWarnings(future::plan(future::multiprocess))
-  mtcars <- read.csv("data/mtcars.csv")
   b <- blbglm(mpg ~ wt, data = mtcars, B = 100)
   expect_s3_class(b, "blbglm")
   co <- coef(b)
@@ -18,18 +11,7 @@ test_that("parallelized, data given directly", {
   expect_type(sigma(b), "double")
 })
 
-test_that("sequential, data read from files", {
-  future::plan(future::sequential)
-  filepaths = c("data/mtcars1.csv", "data/mtcars2.csv")
-  expect_warning(b <- blbglm(mpg ~ wt, filepaths = filepaths, B = 100))
-  expect_s3_class(b, "blbglm")
-  co <- coef(b)
-  expect_equal(length(co), 2)
-  expect_type(sigma(b), "double")
-})
-
 test_that("parallelized, data read from files", {
-  suppressWarnings(future::plan(future::multiprocess))
   filepaths = c("data/mtcars1.csv", "data/mtcars2.csv")
   b <- blbglm(mpg ~ wt, filepaths = filepaths, m = 2, B = 100)
   expect_s3_class(b, "blbglm")
@@ -38,23 +20,26 @@ test_that("parallelized, data read from files", {
   expect_type(sigma(b), "double")
 })
 
-test_that("respects use_plan = FALSE", {
-  future::plan(future::sequential)
-  filepaths = c("data/mtcars1.csv", "data/mtcars2.csv")
-  b <- blbglm(mpg ~ wt, filepaths = filepaths, m = 2, B = 100, use_plan = FALSE)
+future::plan(future::sequential)
+
+test_that("sequential, data given directly", {
+  expect_warning(b <- blbglm(mpg ~ wt, data = mtcars, B = 100))
   expect_s3_class(b, "blbglm")
+  co <- coef(b)
+  expect_equal(length(co), 2)
+  expect_type(sigma(b), "double")
 })
 
-test_that("respects file split over m", {
-  future::plan(future::sequential)
+test_that("sequential, data read from files", {
   filepaths = c("data/mtcars1.csv", "data/mtcars2.csv")
-  expect_warning(b <- blbglm(mpg ~ wt, filepaths = filepaths, B = 100, use_plan = FALSE))
+  expect_warning(b <- blbglm(mpg ~ wt, filepaths = filepaths, B = 100))
   expect_s3_class(b, "blbglm")
+  co <- coef(b)
+  expect_equal(length(co), 2)
+  expect_type(sigma(b), "double")
 })
 
 test_that("logistic regression works", {
-  future::plan(future::sequential)
-  iris <- read.csv("data/iris_subset.csv")
   expect_warning(b <- blbglm(Species ~ Sepal.Length, family = binomial, data = iris, B = 100, use_plan = FALSE))
   expect_s3_class(b, "blbglm")
   co <- coef(b)
@@ -63,8 +48,6 @@ test_that("logistic regression works", {
 })
 
 test_that("multivariate formula works", {
-  future::plan(future::sequential)
-  iris <- read.csv("data/iris_subset.csv")
   expect_warning(b <- blbglm(Species ~ Sepal.Length + Sepal.Width, family = binomial, data = iris, B = 100, use_plan = FALSE))
   expect_s3_class(b, "blbglm")
   co <- coef(b)
