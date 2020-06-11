e9756def8a470558cd5918916f4b1852416dc6b4
Anmei Liu <anmliu@ucdavis.edu>
2020-06-10 18:14:41 -0700

    documentation!

--- R/blblm.R
+++ R/blblm.R
@@ -13,7 +13,61 @@
 utils::globalVariables(c("."))
 
 
+#' Bag of Little Bootstraps General Linear Model
+#'
+#' This function implements the bag of little bootstraps for the general linear
+#' model.
+#'
+#' Linear and logistic regression are properly supported, but if your
+#' data is appropriately formed, there's no reason you can't use other general
+#' linear models with blbglm(). (Corrections to predictions, where necessary,
+#' are only made for the logistic case.)
+#'
+#' Parallelized processing is supported through future::plan(). blbglm()
+#' defaults to using future_map(), respecting any plan() the user has set.
+#' However, disabling the use of future_map() for sequential processing is
+#' usually faster; set use_plan = FALSE for this.
+#'
+#' blbglm() can also work with filepaths in lieu of a data object. Provide a
+#' vector of filepaths and blbglm() will read them for you. (Filepath reading
+#' is faster when blbglm() is running in a parallelized mode.) In this case,
+#' each file is considered to be one subsample (and m is ignored).
+#'
+#' By default, blbglm() assigns an equal number of observations to each
+#' subsample (ties are broken in favor of earlier numbered subsamples). It is
+#' possible to disable this behavior so that blbglm() assigns each observation
+#' to a subsample at random, without regard for the number of observation in
+#' each subsample. The minimum number of observations in each sample is
+#' controlled by min_subsample_size, but it should be at least 2 + the number
+#' of dependent variables.
+#'
+#' @param formula The formula to use for the linear models.
+#' @param family The family of the linear models (see glm()). Defaults to
+#'   gaussian() for linear regression.
+#' @param data An object containing data (which can be subsetted and passed to
+#'   glm()).
+#' @param filepaths A vector or list of filepaths to read from.
+#' @param read_function The function to use to read files.
+#' @param m The number of subsamples to create.
+#' @param B The number of bootstraps for each subsample.
+#' @param min_subsample_size The minimum size of each subsample. For small
+#'   numbers of observations per subsample, mitigates weird behaviors from
+#'   fitting glm() on small data sets.
+#' @param even_split Whether to split subsamples so that they are as equally
+#'   sized as possible.
+#' @param use_plan Whether to use the plan set by future::plan(). use_plan =
+#'   FALSE is faster for sequential processing.
+#' @param ... Additional arguments to pass to read_function.
+#'
+#' @return A blbglm object, containing the estimates from each bootstrap, the
+#'   formula, and the family.
+#'
 #' @export
+#'
+#' @examples
+#' blbglm(mpg ~ wt, data = mtcars, B = 100, even_split = TRUE, use_plan = FALSE)
+#' blbglm(Species ~ Sepal.Length, family = binomial(), data = iris,
+#' m = 3, B = 100, min_subsample_size = 30)
 blbglm <- function(formula, family = gaussian(), data = NULL, filepaths = NULL, read_function = read.csv, m = 10, B = 5000, min_subsample_size = NULL, even_split = NULL, use_plan = TRUE, ...) {
   if (is.null(data) & is.null(filepaths)) {
     stop("Neither data nor filepaths to data provided")
@@ -75,7 +129,14 @@ blbglm <- function(formula, family = gaussian(), data = NULL, filepaths = NULL, 
   invisible(res)
 }
 
-#' split data into m parts of approximated equal sizes
+#' Randomly split data into m parts
+#'
+#' @param data A data object (dataframe, vector, etc.) to split.
+#' @param m The number of splits to create.
+#' @param min_subsample_size The minimum size of each split.
+#' @param even_split Whether to create splits so they are as similarly-sized as possible.
+#'
+#' @return A list containing m subsets of data.
 split_sample <- function(data, m, min_subsample_size, even_split) {
   if (even_split) {
     idx <- sample(rep_len(1:m, NROW(data)))
@@ -87,20 +148,36 @@ split_sample <- function(data, m, min_subsample_size, even_split) {
   data %>% split(idx)
 }
 
-#' compute the estimates
+#' Perform bootstraps with glm()
+#'
+#' @param formula The formula to use for the linear models.
+#' @param family The family of the linear models (see glm()).
+#' @param data An object containing data (which can be passed to glm()).
+#' @param n The number of observations in the data.
+#' @param B The number of bootstraps.
 glm_each_subsample <- function(formula, family, data, n, B) {
   replicate(B, glm_each_boot(formula, family, data, n), simplify = FALSE)
 }
 
 
-#' compute the regression estimates for a blb dataset
+#' Efficiently compute the estimate of glm() parameters for one bootstrap
+#'
+#' @param formula The formula to use for the linear models.
+#' @param family The family of the linear models (see glm()).
+#' @param data An object containing data (which can be passed to glm()).
+#' @param n The number of observations in the data.
 glm_each_boot <- function(formula, family, data, n) {
   freqs <- rmultinom(1, n, rep(1, nrow(data)))
   glm1(formula, family, data, freqs)
 }
 
 
-#' estimate the regression estimates based on given the number of repetitions
+#' Efficiently estimate glm() parameters based on observation frequency
+#'
+#' @param formula The formula to use for the linear models.
+#' @param family The family of the linear models (see glm()).
+#' @param data An object containing data (which can be passed to glm()).
+#' @param freqs The frequency of each observation.
 glm1 <- function(formula, family, data, freqs) {
   # drop the original closure of formula,
   # otherwise the formula will pick a wront variable from the global scope.
@@ -110,17 +187,28 @@ glm1 <- function(formula, family, data, freqs) {
 }
 
 
-#' compute the coefficients from fit
+#' Extract the coefficients from a model fit
+#'
+#' @param fit The model fit to be extracted from.
 blbcoef <- function(fit) {
   coef(fit)
 }
 
 
-#' compute sigma from fit
-blbsigma <- function(fit, freqs) {
+#' Compute sigma based on a model fit and observation weights
+#'
+#' The sigma computed here is based on the efficient bootstrap computation used
+#' above, i.e. it takes into account the weights as separate observations.
+#' fit$weights isn't used because it provides misleading results for
+#' non-Gaussian GLMs, but fit$residuals is used to provide residuals that
+#' generalize between GLM forms.
+#'
+#' @param fit The model fit to be extracted from.
+#' @param weights The weight of each observation in the model.
+blbsigma <- function(fit, weights) {
   p <- fit$rank
   e <- fit$residuals
-  w <- freqs
+  w <- weights
   sqrt(sum(w * (e^2)) / (sum(w) - p))
 }
 
@@ -178,7 +266,7 @@ confint.blbglm <- function(object, parm = NULL, level = 0.95, ...) {
 #' @export
 #' @method predict blbglm
 predict.blbglm <- function(object, new_data, confidence = FALSE, level = 0.95, ...) {
-  X <- model.matrix(reformulate(attr(terms(object$formula), data = new_data, "term.labels")), new_data)
+  X <- model.matrix(reformulate(attr(terms.formula(object$formula, data = new_data), "term.labels")), new_data)
   logit <- ifelse(class(object$fit) == "function", formals(object$fit)$link == "logit",
                   ifelse(class(object$fit) == "family", object$fit$link == "logit", FALSE))
   if (logit) {
